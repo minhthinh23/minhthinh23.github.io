7872866eee1fe940653398380a949016816cb3f9
Ashmit Sachan <ashmit666@gmail.com>
2021-06-20 03:28:33 +0530

    Index Page completed

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -161,3 +161,10 @@ $(document).ready(function () {
 
   navControll();
 });
+
+$(".question").click(function(){ 
+  //console.log("click click!");
+  $(this).toggleClass("question-active");
+  $(this).next().slideToggle(700);
+  $(this).children("img.arrow").toggleClass("arrow-active");
+});
